bdecb09c28c796b13b1d72bec6ad6ad9398597ad
viile <dakgzhu@foxmail.com>
2017-06-02 15:38:32 +0800

    add url parser

--- source/app.d
+++ source/app.d
@@ -1,15 +1,21 @@
 import std.stdio;
 
 import ddo.database;
+import ddo.driver.mysql;
 
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
-	auto db = new MySql("10.1.11.31","dev","111111","blog",3306);
-	writeln(db.error());
-	writeln(db.pingMysql());
-	writeln(db.clientInfo());
-	writeln(db.insert("user",["username":"1"]));
-	db.close();
-	writeln("close");
+	auto mysql = mysql_init(null);
+	char value = 1; 
+	mysql_options(mysql, mysql_option.MYSQL_OPT_RECONNECT, cast(char*)&value);
+	size_t read_timeout = 60;
+	mysql_options(mysql, mysql_option.MYSQL_OPT_READ_TIMEOUT, cast(size_t*)&read_timeout);
+	mysql_options(mysql, mysql_option.MYSQL_OPT_WRITE_TIMEOUT, cast(size_t*)&read_timeout);
+	mysql_real_connect(mysql, toCstring("10.1.11.31"), toCstring("dev"),
+			toCstring("111111"), toCstring("blog"), 3306, null, 0);
+	mysql_query(mysql, toCstring("SET NAMES 'utf8'"));
+	string sql = "select * from user;";
+	mysql_query(mysql, toCstring(sql));
+	writeln(mysql_store_result(mysql), sql);
 }
--- source/ddo/database/mysql.d
+++ source/ddo/database/mysql.d
@@ -12,6 +12,13 @@ import ddo.utils.utils;
 
 class MySql : Database 
 {
+	public string dbname;
+	private string _host;
+	private string _user;
+	private string _pass;
+	private string _db;
+	private uint _port;
+
 	this(string host, string user, string pass, string db, uint port) 
 	{
 		this._port = port;
@@ -19,15 +26,12 @@ class MySql : Database
 		this._user = user;
 		this._db = db;
 		this._pass = pass;
-		dbname = db;
-		this.connect();
+		this.dbname = db;
+		connect();
 	}
 
-	string dbname;
-	private{
-		string _host, _user, _pass, _db;
-		uint _port;
-
+	~this() 
+	{
 	}
 
 	private void connect()
@@ -49,7 +53,8 @@ class MySql : Database
 
 	}
 
-	override void startTransaction() {
+	override void startTransaction() 
+	{
 		query("START TRANSACTION");
 	}
 
@@ -58,11 +63,13 @@ class MySql : Database
 		return fromCstring(mysql_get_client_info());
 	}
 
-	string error() {
+	string error() 
+	{
 		return fromCstring(mysql_error(mysql));
 	}
 
-	void close() {
+	void close() 
+	{
 		try{
 			mysql_close(mysql);
 		}catch(Exception ex)
@@ -70,198 +77,30 @@ class MySql : Database
 		}
 	}
 
-	~this() {
-	}
-
-	///ping mysql
 	int pingMysql()
 	{
 		return mysql_ping(mysql);
 	}
 
-	///
 	size_t getThreadId()
 	{
 		return mysql_thread_id(mysql);
 	}
 
-	public int lastInsertId() {
+	public int lastInsertId() 
+	{
 		return cast(int) mysql_insert_id(mysql);
 	}
 
-	int insert(string table, MySqlResult result, string[string] columnsToModify, string[] columnsToSkip) {
-		assert(!result.empty);
-		string sql = "INSERT INTO `" ~ table ~ "` ";
-
-		string cols = "(";
-		string vals = "(";
-		bool outputted = false;
-
-		string[string] columns;
-		auto cnames = result.fieldNames;
-		foreach(i, col; result.front.toStringArray) {
-			bool skipMe = false;
-			foreach(skip; columnsToSkip) {
-				if(cnames[i] == skip) {
-					skipMe = true;
-					break;
-				}
-			}
-			if(skipMe)
-				continue;
-
-			if(outputted) {
-				cols ~= ",";
-				vals ~= ",";
-			} else
-				outputted = true;
-
-			cols ~= cnames[i];
-
-			if(result.columnIsNull[i] && cnames[i] !in columnsToModify)
-				vals ~= "NULL";
-			else {
-				string v = col;
-				if(cnames[i] in columnsToModify)
-					v = columnsToModify[cnames[i]];
-
-				vals ~= "'" ~ escape(v) ~ "'"; 
-
-			}
-		}
-
-		cols ~= ")";
-		vals ~= ")";
-
-		sql ~= cols ~ " VALUES " ~ vals;
-
-		query(sql);
-
-		result.popFront;
-
-		return lastInsertId;
-	}
-
-	int insert(string table,  string[string] columnsToModify) {
-		string sql = "INSERT INTO `" ~ table ~ "` ";
-
-		string cols = "(";
-		string vals = "(";
-		bool outputted = false;
-
-		string[string] columns;
-		foreach(i, col; columnsToModify) 
-		{
-			if(outputted) {
-				cols ~= ",";
-				vals ~= ",";
-			} 
-			else{
-
-				outputted = true;
-			}
-			cols ~= "`" ~i~ "`";
-			vals ~= "'" ~ escape(col) ~ "'"; 
-
-		}
-
-		cols ~= ")";
-		vals ~= ")";
-
-		sql ~= cols ~ " VALUES " ~ vals;
-
-		query(sql);
-		return lastInsertId;
-	}
-
-	///更新
-	int update(string table,  string[string] columnsToModify, string[string] whereCondition) {
-		import std.array;
-		Appender!string sql = appender!string();
-		//UPDATE  `sendmsg` SET `used_time` = '%s' WHERE `id` = '%s' LIMIT 1;"
-		sql.put("UPDATE  `");
-		sql.put(table);
-		sql.put("` SET ");
-		string[] sets, wheres;
-		foreach(i, col; columnsToModify) 
-		{
-			sets~= "`" ~i~ "`=" ~"'" ~ escape(col) ~ "'";
-		}
-		foreach(i, col; whereCondition) 
-		{
-			wheres~= "`" ~i~ "`=" ~"'" ~ escape(col) ~ "'";
-		}
-		sql.put(sets.join(", "));
-		if(wheres.length != 0 )
-		{
-			sql.put(" WHERE ");
-			sql.put(wheres.join(" AND "));
-		}
-		query(sql.data);
-		return 1;
-	}
-
-	///删除
-	int remove(string table,   string[string] whereCondition) {
-		import std.array;
-		Appender!string sql = appender!string();
-		sql.put("DELETE FROM  `");
-		sql.put(table);
-		sql.put("` ");
-		string[] wheres;
-
-		foreach(i, col; whereCondition) 
-		{
-			wheres~= "`" ~i~ "`=" ~"'" ~ escape(col) ~ "'";
-		}
-		if(wheres.length != 0 )
-		{
-			sql.put(" WHERE ");
-			sql.put(wheres.join(" AND "));
-		}
-		query(sql.data);
-		return 1;
-	}
-
-	///查询
-	//sqlInstance.sqlClient.update("user", ["id"], ["id":"1"], 1);
-	ResultSet select(string table,  string[] columnsToSelected, string[string] whereCondition, int limit = 1) {
-		import std.array;
-		Appender!string sql = appender!string();
-		sql.put("SELECT ");
-		sql.put(columnsToSelected.join(", "));
-		sql.put(" FROM `");
-		sql.put(table);
-		sql.put("` ");
-
-		string[] wheres;
-
-		foreach(i, col; whereCondition) 
-		{
-			wheres ~= "`" ~i~ "`=" ~"'" ~ escape(col) ~ "'";
-		}
-		if(wheres.length != 0 )
-		{
-			sql.put(" WHERE ");
-			sql.put(wheres.join(" AND "));
-		}
-		if(limit > 0)
-		{
-			sql.put(" LIMIT ");
-
-			sql.put(to!string(limit));
-		}
-		return query(sql.data);
-	}
-
-	string escape(string str) {
+	string escape(string str) 
+	{
 		ubyte[] buffer = new ubyte[str.length * 2 + 1];
 		buffer.length = mysql_real_escape_string(mysql, buffer.ptr, cast(cstring) str.ptr, cast(uint) str.length);
-
 		return cast(string) buffer;
 	}
 
-	string escaped(T...)(string sql, T t) {
+	string escaped(T...)(string sql, T t) 
+	{
 		static if(t.length > 0) {
 			string fixedup;
 			int pos = 0;
@@ -298,14 +137,14 @@ class MySql : Database
 
 			sql = fixedup;
 
-			//writefln("\n\nExecuting sql: %s", sql);
 		}
 
 		return sql;
 	}
 
 
-	ResultByDataObject!R queryDataObject(R = DataObject, T...)(string sql, T t) {
+	ResultByDataObject!R queryDataObject(R = DataObject, T...)(string sql, T t) 
+	{
 		// modify sql for the best data object grabbing
 		sql = fixupSqlForDataObjectUse(sql);
 		//import std.stdio;
@@ -316,28 +155,26 @@ class MySql : Database
 	}
 
 
-	ResultByDataObject!R queryDataObjectWithCustomKeys(R = DataObject, T...)(string[string] keyMapping, string sql, T t) {
+	ResultByDataObject!R queryDataObjectWithCustomKeys(R = DataObject, T...)
+		(string[string] keyMapping, string sql, T t) 
+	{
 		sql = fixupSqlForDataObjectUse(sql, keyMapping);
 
 		auto magic = query(sql, t);
 		return ResultByDataObject!R(cast(MySqlResult) magic, this);
 	}
 
-	int affectedRows() {
+	int affectedRows() 
+	{
 		return cast(int) mysql_affected_rows(mysql);
 	}
 
-	override ResultSet queryImpl(string sql, Variant[] args...) {
+	override ResultSet queryImpl(string sql, Variant[] args...) 
+	{
 		import std.stdio;
 
-		//writeln("query before....", sql);
 		sql = escapedVariants(this, sql, args);
-		//writeln("query after....",sql);
 
-		/*enforceEx!(DatabaseException)(
-		  !mysql_query(mysql, toCstring(sql)),
-		  error() ~ " :::: " ~ sql);
-		 */
 		try{
 			mysql_query(mysql, toCstring(sql));
 		}
@@ -345,7 +182,6 @@ class MySql : Database
 		{
 			std.stdio.writeln( ex.msg, " :::: " , sql);
 		}
-		//writeln("query tocstring....",toCstring(sql));
 		return new MySqlResult(mysql_store_result(mysql), sql);
 	}
 
